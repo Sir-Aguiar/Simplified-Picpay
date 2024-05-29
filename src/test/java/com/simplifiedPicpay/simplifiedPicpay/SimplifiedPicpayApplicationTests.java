e6b82107c901eca2a75f5bb4e72e1e50a387357c
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-05-29 16:30:04 -0400

    Initial Commit

---
+++ src/test/java/com/simplifiedPicpay/simplifiedPicpay/SimplifiedPicpayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.simplifiedPicpay.simplifiedPicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
